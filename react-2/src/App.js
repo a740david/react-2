c2e89d1850c3567c439aac5c54741931d58fc853
a740david <54502632+a740david@users.noreply.github.com>
2023-03-20 12:33:40 +0200

    Initial commit

---
+++ react-2/src/App.js
@@ -0,0 +1,29 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// fonts import
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+import SearchForm from './SolutionReact2_1/SearchForm.js';
+
+
+
+
+export default function App() {
+
+  console.log("Rendering App")
+
+ 
+
+   return <SearchForm />
+
+ 
+
+
+}
+
+
+
+// export default App;
